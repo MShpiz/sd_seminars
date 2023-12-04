707d598331ee456127fa3702037cbc767ffdb38e
MShpiz <reg@panfilova.com>
2023-12-04 18:16:03 +0300

    task4

--- seminar_4/task4/src/main/kotlin/main.kt
+++ seminar_4/task4/src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
+    var num = readLine()?.toInt() ?: 0
+    if (num < 0) {num = 0}
+    val arr: Array<Array<Int>> = Array(num) { Array(num) {(-100..100).random()} }
+    for (i in 0 until num) {
+        for (j in 0 until num) {
+            print("${arr[i][j]} ")
+        }
+        println()
+    }
 
+    var sum = 0
+    for (i in 0 until num) {
+        for (j in 0 until num) {
+            if (i + j >= num) {
+                sum += arr[i][j]
+            }
+        }
+    }
+    println(sum)
 }
